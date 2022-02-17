35707415b5fb67a6f73d7e96525a05db2eb38d50
wakehood <99768292+wakehood@users.noreply.github.com>
2022-02-16 21:25:03 -0800

    firt commit

---
+++ FileSnooper/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FileSnooper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string dirPath = @"/Users/wakehood/Pictures";
+
+
+            string[] dirfiles = Directory.GetFiles(dirPath);
+
+            foreach(string filename in dirfiles)
+            {
+                Console.WriteLine(filename);
+            }
+
+            String searchFolder = dirPath;
+            var filters = new String[] { "jpg", "jpeg", "png", "gif", "tiff", "bmp", "svg" };
+            var files = GetFilesFrom(searchFolder, filters, false);
+        }
+
+        public static String[] GetFilesFrom(String searchFolder, String[] filters, bool isRecursive)
+        {
+            List<String> filesFound = new List<String>();
+            var searchOption = isRecursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+            foreach (var filter in filters)
+            {
+                filesFound.AddRange(Directory.GetFiles(searchFolder, String.Format("*.{0}", filter), searchOption));
+            }
+            return filesFound.ToArray();
+        }
+    }
+}
